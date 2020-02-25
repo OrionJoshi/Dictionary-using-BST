20c276f5aa7ec47d4da9706d1a628fbea3c806ab
khageshwor <khageshwarjoshi03@gmail.com>
2020-02-25 07:26:18 -0800

    different function added

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -58,4 +58,136 @@ int main(void) {
         }
         return 0;
   }
+  struct BSTnode * createNode(char *word, char *meaning) {
+        struct BSTnode *newnode;
+        newnode = (struct BSTnode *)malloc(sizeof(struct BSTnode));
+        strcpy(newnode->word, word);
+        strcpy(newnode->meaning, meaning);
+        newnode->left = newnode->right = NULL;
+        return newnode;
+  }
+
+  void insert(char *word, char *meaning) {
+        struct BSTnode *parent = NULL, *current = NULL, *newnode = NULL;
+        int res = 0;
+        if (!root) {
+                root = createNode(word, meaning);
+                return;
+        }
+        for (current = root; current !=NULL;
+           current = (res > 0) ? current->right : current->left) {
+                res = strcasecmp(word, current->word);
+                if (res == 0) {
+                        printf("Duplicate entry!!\n");
+                        return;
+                }
+                parent = current;
+        }
+        newnode = createNode(word, meaning);
+        res > 0 ? (parent->right = newnode) : (parent->left = newnode);
+        return;
+  }
+
+  void deleteNode(char *str) {
+        struct BSTnode *parent = NULL, *current = NULL, *temp = NULL;
+        int flag = 0, res = 0;
+        if (!root) {
+                printf("BST is not present!!\n");
+                return;
+        }
+        current = root;
+        while (1) {
+                res = strcasecmp(current->word, str);
+                if (res == 0)
+                        break;
+                flag = res;
+                parent = current;
+                current = (res > 0) ? current->left : current->right;
+                if (current == NULL)
+                        return;
+        }
+        /* deleting leaf node */
+        if (current->right == NULL) {
+                if (current == root && current->left == NULL) {
+                        free(current);
+                        root = NULL;
+                        return;
+                } else if (current == root) {
+                        root = current->left;
+                        free (current);
+                        return;
+                }
+
+                flag > 0 ? (parent->left = current->left) :
+                                (parent->right = current->left);
+        } else {
+                /* delete node with single child */
+                temp = current->right;
+                if (!temp->left) {
+                        temp->left = current->left;
+                        if (current == root) {
+                                root = temp;
+                                free(current);
+                                return;
+                        }
+                        flag > 0 ? (parent->left = temp) :
+                                        (parent->right = temp);
+                } else {
+                        /* delete node with two children */
+                        struct BSTnode *successor = NULL;
+                        while (1) {
+                                successor = temp->left;
+                                if (!successor->left)
+                                        break;
+                                temp = successor;
+                        }
+                        temp->left = successor->right;
+                        successor->left = current->left;
+                        successor->right = current->right;
+                        if (current == root) {
+                                root = successor;
+                                free(current);
+                                return;
+                        }
+                        (flag > 0) ? (parent->left = successor) :
+                                        (parent->right = successor);
+                }
+        }
+        free (current);
+        return;
+  }
+
+  void findElement(char *str) {
+        struct BSTnode *temp = NULL;
+        int flag = 0, res = 0;
+        if (root == NULL) {
+                printf("Binary Search Tree is out of station!!\n");
+                return;
+        }
+        temp = root;
+        while (temp) {
+                if ((res = strcasecmp(temp->word, str)) == 0) {
+                        printf("Word   : %s", str);
+                        printf("Meaning: %s", temp->meaning);
+                        flag = 1;
+                        break;
+                }
+                temp = (res > 0) ? temp->left : temp->right;
+        }
+        if (!flag)
+                printf("Search Element not found in Binary Search Tree\n");
+        return;
+  }
+
+  void inorderTraversal(struct BSTnode *myNode) {
+        if (myNode) {
+                inorderTraversal(myNode->left);
+                printf("Word    : %s", myNode->word);
+                printf("Meaning : %s", myNode->meaning);
+                printf("\n");
+                inorderTraversal(myNode->right);
+        }
+        return;
+  }
+
 
